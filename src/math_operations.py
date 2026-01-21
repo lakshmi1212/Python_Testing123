b040efc81fd7493051afb2fedd1800012ec4fc4d
lakshmi1212 <36476419+lakshmi1212@users.noreply.github.com>
2026-01-21 21:59:40 +0530

    Add or update file: math_operations.py

--- src/math_operations.py
+++ src/math_operations.py
@@ -1,23 +1,7 @@
-# math_operations.py
-
 def add(a, b):
-    """
-    Returns the sum of a and b.
-    Args:
-        a (int|float): First number
-        b (int|float): Second number
-    Returns:
-        int|float: Sum of a and b
-    """
+    """Return the sum of a and b."""
     return a + b
 
 def subtract(a, b):
-    """
-    Returns the result of subtracting b from a.
-    Args:
-        a (int|float): First number
-        b (int|float): Second number
-    Returns:
-        int|float: Difference of a and b
-    """
+    """Return the difference of a and b."""
     return a - b
